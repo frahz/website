04c615782571934b776624c6ffaf37a6b98aca84
frahz <me@frahz.dev>
2025-07-30 18:47:56 -0700

    chore(nix): adjust flake.nix

--- flake.nix
+++ flake.nix
@@ -3,30 +3,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-    forEachSystem = nixpkgs.lib.genAttrs systems;
-    pkgsForEach = forEachSystem (system:
-      import nixpkgs {
-        inherit system;
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [
+            pkgs.hugo
+          ];
+        };
       });
-  in {
-    devShells = forEachSystem (system: let
-      pkgs = pkgsForEach.${system};
-    in {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.hugo
-        ];
-      };
-    });
-  };
+    };
 }
